8991dd153a1fc70dfc4c873c357ea8dd19f61f54
Nas WR <45099982+Nasseer125@users.noreply.github.com>
2023-09-13 11:23:10 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,10 +15,26 @@
 import streamlit as st
 from streamlit.logger import get_logger
  
-st.write("""
-# My first app
-Hello *world!*
-""")
+# Charger le modèle hybride
+hybrid_model = joblib.load("svdpp_model.pkl")
 
+# Interface utilisateur Streamlit
+st.title("Système de recommandation de films")
+
+# Formulaire de saisie utilisateur
+user_id = st.text_input("Entrez l'ID de l'utilisateur:")
+movie_id = st.text_input("Entrez l'ID du film:")
+
+if user_id and movie_id:
+    try:
+        user_id = int(user_id)
+        movie_id = int(movie_id)
+
+        # Faire une recommandation en utilisant le modèle hybride
+        prediction = hybrid_model.estimate(user_id, movie_id)
+
+        st.write(f"La prédiction de la note pour l'utilisateur {user_id} et le film {movie_id} est {prediction:.2f}")
+    except ValueError:
+        st.write("Veuillez entrer des valeurs numériques valides.")
 
 
